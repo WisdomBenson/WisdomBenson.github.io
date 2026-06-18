f03ca9f57636907ce4eb8c51936ff1e5b479c1de
Wisemanking001 <wbenson@eagles.nccu.edu>
2026-06-18 10:46:39 -0400

    Update portfolio for WisdomBenson ownership in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/WisdomBenson.github.io/',
+  base: '/',
   build: {
     chunkSizeWarningLimit: 900,
     rollupOptions: {
